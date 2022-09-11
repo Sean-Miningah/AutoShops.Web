6c54574ee3dede4a11f3b6952cbe8313a48d198e
Sean-Miningah <miningahsean@gmail.com>
2022-09-11 21:46:45 +0300

    Media files settings

--- config/settings.py
+++ config/settings.py
@@ -173,6 +173,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Media files 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
+
 # Default primary key field type
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
