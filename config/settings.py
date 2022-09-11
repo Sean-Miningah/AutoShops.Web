9ce6eb01fc50be2479587a66c5654b5036f0994a
Sean-Miningah <miningahsean@gmail.com>
2022-09-11 20:00:15 +0300

    Dockerise django api with postgres db server and pgadmin as db client

--- config/settings.py
+++ config/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': env('POSTGRES_DB'),
         'USER': env('POSTGRES_USER'),
         'PASSWORD': env('POSTGRES_PASSWORD'),
