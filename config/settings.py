b44d9fd6e4f3c5b1d9ec53c5c9d67410e1da449b
Sean-Miningah <miningahsean@gmail.com>
2022-11-15 22:21:04 +0300

    graphql and django_filters settings removal

--- config/settings.py
+++ config/settings.py
@@ -1,7 +1,6 @@
 import environ
 import os
 from datetime import timedelta
-from graphql_auth.settings import DEFAULTS
 
 env = environ.Env(
     DEBUG=(bool, False)
@@ -13,19 +12,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Take environment variables from .env file
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
 CORS_ORIGIN_ALLOW_ALL = True
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -39,10 +32,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'drf_yasg',
-    'graphene_django',
-    'graphql_auth',
-    'graphql_jwt.refresh_token.apps.RefreshTokenConfig',
-    'django_filters',
     'channels',
 
     'autouser',
@@ -82,9 +71,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 ASGI_APPLICATION = 'config.asgi.application'
 
-
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
 if not DEBUG:
     DATABASES = {
         'default': {
@@ -109,7 +97,6 @@ else:
         }
     }
 
-
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
@@ -130,37 +117,14 @@ GRAPHENE = {
     ],
 }
 
-GRAPHQL_JWT = {
-    'JWT_VERIFY_EXPIRATION': True,
-    'JWT_LONG_RUNNING_REFRESH_TOKEN': True,
-    "JWT_ALLOW_ANY_CLASSES": [
-        "graphql_auth.mutations.Register",
-        "graphql_auth.mutations.VerifyAccount",
-        "graphql_auth.mutations.ObtainJSONWebToken",
-    ]
-}
-
-DEFAULTS['LOGIN_ALLOWED_FIELDS'] = ['email']
-DEFAULTS['REGISTER_MUTATION_FIELDS'] = ['email', 'phone_number', 
-        'is_advertiser', 'photo', 'first_name', 'last_name', 'is_technician',]
-DEFAULTS['USER_NODE_FILTER_FIELDS'] = {
-    'email': ['exact'],
-    'is_active': ['exact'],
-    'status__archived': ['exact'],
-    'status__verified': ['exact'],
-    'status__secondary_email': ['exact'],
-}
-GRAPHQL_AUTH = DEFAULTS
-
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
-    "graphql_auth.backends.GraphQLAuthBackend",
 ]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
 }
 
 SIMPLE_JWT = {
@@ -221,7 +185,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Default user field
 AUTH_USER_MODEL = "autouser.AutoUser"
 
-# Gmail Serivices
+# Gmail Services
 DEFAULT_FROM_EMAIL = env('EMAIL_HOST_USER')
 
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.decorators.csrf import csrf_exempt
-from graphene_django.views import GraphQLView
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -24,7 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
     path('api/auto-users/', include('autouser.urls')),
     # path('api/technicians/', include('technician.urls')),
 ]
