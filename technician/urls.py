8dff2b939d40b95768763222abd465774bb150c0
Sean-Miningah <miningahsean@gmail.com>
2022-11-21 00:59:49 +0300

    Introduced bookigns url

--- technician/urls.py
+++ technician/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from technician.views import (TechnicianRegisterView, TechnicianLoginView, TechnicianView, TechnicianSpecializationView,
-                              TechnicianFeedbackView, TechnicianOnBoardingView, SpecializationsView, TechnicianFeedView)
+                              TechnicianFeedbackView, TechnicianOnBoardingView, SpecializationsView, TechnicianFeedView,
+                              TechnicianBookingsView)
 
 
 router = DefaultRouter()
@@ -11,10 +12,11 @@ router.register("register", TechnicianRegisterView, basename="technician-registr
 router.register("login", TechnicianLoginView, basename="technician-login")
 router.register("details", TechnicianView, basename="technician-details")
 router.register("specializations", TechnicianSpecializationView, basename="technician-specialization")
-router.register("feedback", TechnicianFeedbackView, basename="technician-feedback")
+router.register("reviews", TechnicianFeedbackView, basename="technician-feedback")
 router.register("onboarding", TechnicianOnBoardingView, basename="technician-onboarding")
 router.register("technician-specializations", SpecializationsView, basename="technician-specializations")
 router.register("technician-feed", TechnicianFeedView, basename="technician-feed")
+router.register("bookings", TechnicianBookingsView, basename="technician-bookings")
 
 urlpatterns = [
     path('', include(router.urls)),
